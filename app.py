1b13fa75478d145fc9a9aa05a1dd0e8ed08f5cbd
Ssahoo77 <sneha.sahoo2004@gmail.com>
2025-06-28 15:16:33 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Welcome To The Churn Prediction Model')
+st.write('If you want to predict the Churn Probability Navigate to "Classification"')
+st.write('If you want to predict the Salary Navigate to "Regression"')
